8eb0f5705fc7b88acfd2c2264af72a1d9a4fb70e
Cleyton Caminha <cleytoncaminha01@gmail.com>
2023-07-05 14:42:23 -0300

    start todo crud

---
+++ core/crud.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+const DB_FILE_PATH = "./core/db"
+
+console.log("CRUD")
+
+function create(content) {
+    fs.writeFileSync(DB_FILE_PATH, content)
+    return content
+}
+
+console.log(create("ola mundo de novo"))
