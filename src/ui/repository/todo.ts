99f101798eae23614a95df3e403d26e1c9ba302d
Cleyton Caminha <cleytoncaminha01@gmail.com>
2023-07-08 19:47:37 -0300

    add validations

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -7,21 +7,61 @@ interface TodoRepositoryGetOutput {
     total: number;
     pages: number;
 }
+function get({
+    page,
+    limit,
+}: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
+    return fetch(`/api/todos?page=${page}&limit=${limit}`).then(
+        async (respostaDoServidor) => {
+            const todosString = await respostaDoServidor.text();
+            // Como garantir a tipagem de tipos desconhecidos?
+            const responseParsed = parseTodosFromServer(
+                JSON.parse(todosString)
+            );
 
-function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+            return {
+                total: responseParsed.total,
+                todos: responseParsed.todos,
+                pages: responseParsed.pages,
+            };
+        }
+    );
+}
+
+export const todoRepository = {
+    get,
+};
+
+// Model/Schema
+interface Todo {
+    id: string;
+    content: string;
+    date: Date;
+    done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): {
+    total: number;
+    pages: number;
+    todos: Array<Todo>;
+} {
     if (
         responseBody !== null &&
         typeof responseBody === "object" &&
         "todos" in responseBody &&
+        "total" in responseBody &&
+        "pages" in responseBody &&
         Array.isArray(responseBody.todos)
     ) {
         return {
+            total: Number(responseBody.total),
+            pages: Number(responseBody.pages),
             todos: responseBody.todos.map((todo: unknown) => {
-                if (todo == null && typeof todo !== "object") {
-                    throw new Error("invalid todo from api");
+                if (todo === null && typeof todo !== "object") {
+                    throw new Error("Invalid todo from API");
                 }
 
-                const { id, content, date, done } = todo as {
+                const { id, content, done, date } = todo as {
                     id: string;
                     content: string;
                     date: string;
@@ -39,40 +79,8 @@ function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
     }
 
     return {
+        pages: 1,
+        total: 0,
         todos: [],
     };
-}
-
-function get({
-    page,
-    limit,
-}: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
-    return fetch("/api/todos").then(async (respostaDoServidor) => {
-        const todosString = await respostaDoServidor.text();
-        const todosFromServer = parseTodosFromServer(JSON.parse(todosString));
-
-        const ALL_TODOS = todosFromServer.todos;
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-        const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex);
-        const totalPages = Math.ceil(ALL_TODOS.length / limit);
-
-        return {
-            todos: paginatedTodos,
-            total: ALL_TODOS.length,
-            pages: totalPages,
-        };
-    });
-}
-
-export const todoRepository = {
-    get,
-};
-
-// Model/Schema
-interface Todo {
-    id: string;
-    content: string;
-    date: Date;
-    done: boolean;
 }
